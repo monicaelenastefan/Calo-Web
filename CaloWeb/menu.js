8eab6f8cedc02e02d47bf2ade1396d991bbc25d3
Monica Stefan <monicaelenastefan@gmail.com>
2019-03-18 21:12:51 +0200

    made hover and cursor pointer over ul li elements

---
+++ CaloWeb/menu.js
@@ -0,0 +1,21 @@
+ var cotent = document.getElementById("xx");
+ var button = document.getElementById("Day");
+ 
+ button.onclick= function(){
+    
+        if(content.className == "open") {
+            
+           //shrink to button;
+           content.className= "";
+        }
+        else{
+            //expand box;
+            content.className = "open";
+            
+           
+            
+        }
+     
+     
+     
+ };
